917a493f37edd5c3baae067abb9810f7c92abb5d
Vellamo <vella.kurkela@gmail.com>
2023-05-24 14:04:13 +0300

    Add .gitignore file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from functools import wraps
 
@@ -13,7 +14,7 @@ from flask_gravatar import Gravatar
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('SECRET KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
